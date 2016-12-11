6701490e71fa85a7682e6dc2281978bda009f789
flyinfreedom <flyinfreedeom1124@gmail.com>
2016-12-11 19:01:05 +0800

    First Command

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+/// <reference path="../../typings/index.d.ts" />
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $(".sidebar-nav > li").click(function () {
+      if ($(this).attr("class") != "sidebar-brand") {
+        $(".sidebar-nav > .active").each(function () {
+          $(this).removeClass('active');
+        });
+
+        $(this).addClass('active');
+      }
+    });
+  }
 }
+
+
+
